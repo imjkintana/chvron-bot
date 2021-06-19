02b250693d8e9933e5af037f0a33b7bb021b50f9
Jambles <jkintana@protonmail.com>
2021-06-19 16:28:52 -0700

    added gameinfo functionality

---
+++ gameInfo.py
@@ -0,0 +1,31 @@
+import re, urllib.request
+from bs4 import BeautifulSoup
+
+updateRegex =   re.compile(r'(?<=<p>).*?(?=<p>)')
+versionRegex =  re.compile(r'^.(\S)+')
+dateRegex =     re.compile(r'(?<=\().*?(?=\))')
+noteRegex =     re.compile(r'(?<=\<li\>).*?(?=\<\/li\>)')
+
+def getGameInfo():
+    url = "https://devforum.roblox.com/t/retail-tycoon-2-changelog/858524"
+
+    try:
+        html = urllib.request.urlopen(url).read()
+    except urllib.error.HTTPError:
+        return "HTTP error. Unable to connect."
+
+    soup = BeautifulSoup(html, "html5lib")
+    soup = str(soup)
+    soup = soup.replace("\n", " ")
+
+    allUpdates =    updateRegex.findall(soup)
+    lastUpdate =    allUpdates[1]
+    lastVersion =   versionRegex.search(lastUpdate)
+    lastVersion =   lastVersion.group()
+    lastDate =      dateRegex.search(lastUpdate)
+    lastDate =      lastDate.group()
+    notes =         noteRegex.findall(lastUpdate)
+    notes =         ' '.join([("\n→ " + elem) for elem in notes])
+    infoString =    "Last update: " + lastDate + "\nVersion: " + lastVersion + notes
+
+    return infoString
--- main.py
+++ main.py
@@ -1,20 +1,35 @@
-import discord
-import os
+from discord.ext import commands
 from dotenv import load_dotenv
+from gameInfo import getGameInfo
+import os
 
 load_dotenv('source.env')
-client = discord.Client()
+# client = discord.Client()
+client = commands.Bot(command_prefix = '16.')
+client.lastUpdate = getGameInfo()
 
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
 
+@client.command()
+async def lastupdate(ctx):
+    await ctx.send(client.lastUpdate)
+
+@client.command()
+async def refresh(ctx):
+    client.lastUpdate = getGameInfo()
+    await ctx.send('Refreshed logs.')
+
+'''
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
+    elif message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+        #print(f'In #{message.channel.name}, {message.author.name} triggered "Hello!"')
+'''
 
 client.run(os.getenv('TOKEN'))
